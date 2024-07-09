ac7accbff9a66099105050fc862859f5b5defd0c
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2024-07-08 23:51:11 -0600

    added to MQSettings window
    you can access the toggles in mqsettings. without reloading the game UI
    
    the in game xml window is still there as well.

--- MQ2PluginManager.cpp
+++ MQ2PluginManager.cpp
@@ -14,6 +14,7 @@
 
 	History -
 		MQ2PluginManager created - 08/06/2019
+		Added ImGui Version of window to MQSettings 7/6/2024 ~ Grimmier
 */
 
 
@@ -22,6 +23,7 @@
 #include "CPluginTree.h"
 #include "CPluginToolWnd.h"
 #include "MQ2PluginManager.h"
+#include "imgui/fonts/IconsFontAwesome.h"
 
 PreSetup("MQ2PluginManager");
 PLUGIN_VERSION(2019.0828);
@@ -31,14 +33,17 @@ CPluginToolWnd* PluginWnd = nullptr;
 CPluginTree* PluginTree = nullptr;
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 
+void DrawPluginManager_MQSettingsPanel();
+
 //=-=-=-=-=- Plugin members =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 PLUGIN_API void InitializePlugin()
 {
 	DebugSpewAlways("Initializing MQ2PluginManager");
-	WriteChatColor("\aoLoading MQ2PluginManager v0.0.1 ...");
+	WriteChatColor("\aoLoading MQ2PluginManager v0.0.2 ...");
 	AddCommand("/Pluginman", DoPluginTool, 0, 0, 1);
 	AddXMLFile("MQUI_PluginManagerWnd.xml");
 	PluginTree = new CPluginTree();
+	AddSettingsPanel("plugins/PluginManager", DrawPluginManager_MQSettingsPanel);
 }
 
 PLUGIN_API void ShutdownPlugin()
@@ -56,6 +61,7 @@ PLUGIN_API void ShutdownPlugin()
 	}
 	RemoveCommand("/Pluginman");
 	RemoveXMLFile("MQUI_PluginManagerWnd.xml");
+	RemoveSettingsPanel("plugins/PluginManager");
 }
 
 void CreatePluginWindow()
@@ -91,6 +97,78 @@ void DoPluginTool(PSPAWNINFO pChar, PCHAR szLine)
 	else
 	{
 		PluginWnd->SetVisible(true);
+	}
+}
+
+static MQPlugin* FindMQ2Plugin(const char* szLine)
+{
+	MQPlugin* pPlugin = pPlugins;
+	while (pPlugin)
+	{
+		if (!_stricmp(const_cast<char*>(szLine), pPlugin->szFilename))
+		{
+			return pPlugin;
+		}
+		pPlugin = pPlugin->pNext;
+	}
+	return nullptr;
+}
+
+
+void DrawPluginManager_MQSettingsPanel() {
+	ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "MQ2PluginManager");
+
+	std::vector<CPluginInfo*> pluginList = PluginTree->GetCurrentPluginList();
+
+	ImGui::SeparatorText("Plugins");
+
+	if (ImGui::BeginTable("PluginTable", 2, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg)) {
+		int idx = 0;
+		for (auto& plugin : pluginList) {
+			const char* pluginName = plugin->GetName();
+			bool isLoaded = FindMQ2Plugin(pluginName) != nullptr;
+			ImVec4 textColor = isLoaded ? ImVec4(0.0f, 1.0f, 0.0f, 1.0f) : ImVec4(0.867f, 0.410f, 0.510f, 1.0f);
+
+			if (idx == 0) {
+				ImGui::TableNextRow();
+			}
+
+			ImGui::TableSetColumnIndex(idx);
+			ImGui::PushID(pluginName); // Ensure unique ID for each row
+
+			// Combine plugin name and toggle icon into one string
+			std::string combinedLabel = std::string(isLoaded ? ICON_FA_TOGGLE_ON : ICON_FA_TOGGLE_OFF) + "\t" + (pluginName) ;
+
+			// Create a selectable item with the combined string and color it
+			if (ImGui::Selectable("", false)) {
+				if (strcmp(pluginName, "MQ2PluginManager") == 0) {
+					// Gracefully shutdown MQ2PluginManager
+					ShutdownPlugin();
+					char unloadCmd[MAX_STRING];
+					sprintf_s(unloadCmd, "/plugin %s unload", pluginName);
+					DoCommand(GetCharInfo()->pSpawn, const_cast<char*>(unloadCmd));
+				}
+				else {
+					// Toggle other plugins state
+					if (isLoaded) {
+						char unloadCmd[MAX_STRING];
+						sprintf_s(unloadCmd, "/plugin %s unload", pluginName);
+						DoCommand(GetCharInfo()->pSpawn, const_cast<char*>(unloadCmd));
+					}
+					else {
+						char loadCmd[MAX_STRING];
+						sprintf_s(loadCmd, "/plugin %s", pluginName);
+						DoCommand(GetCharInfo()->pSpawn, const_cast<char*>(loadCmd));
+					}
+				}
+			}
+			ImGui::SameLine();
+			ImGui::TextColored(textColor, "%s", combinedLabel.c_str());
+
+			ImGui::PopID();
+			idx = (idx == 0) ? 1 : 0;
+		}
+		ImGui::EndTable();
 	}
 }
 
